3a16bf3d0582bf79f1fc35227c2d102cbf93277b
Adam Radomski <adam.radomski@inivation.com>
2021-03-23 11:04:47 +0100

    Revert "Added aslam cameras to compilation targets and linked it to calibrator"
    This reverts commit 728f4a58d6afd3787f067154eaa605d7334bf579.

--- src/utilities/calibrator.cpp
+++ src/utilities/calibrator.cpp
@@ -2,7 +2,6 @@
 // Created by radam on 2021-03-23.
 //
 
-#include <aslam/cameras/GridDetector.hpp>
 #include <utilities/calibrator.hpp>
 
 Calibrator::Calibrator() {
@@ -10,8 +9,5 @@ Calibrator::Calibrator() {
 }
 
 cv::Mat Calibrator::process(const cv::Mat &img) {
-
-
-
   return img;
 }
