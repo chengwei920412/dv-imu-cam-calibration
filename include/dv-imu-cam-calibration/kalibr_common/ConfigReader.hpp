9ea2156d35bdcedcde3c329764433f6ed9436b3d
Adam Radomski <adam.radomski@inivation.com>
2021-03-24 11:37:41 +0100

    Added ConfigReader from kalibr_common

---
+++ include/dv-imu-cam-calibration/kalibr_common/ConfigReader.hpp
@@ -0,0 +1,22 @@
+//
+// Created by radam on 2021-03-23.
+//
+
+#pragma once
+
+#include <cstddef>
+
+class AslamCamera {
+
+public:
+
+protected:
+
+
+public:
+
+  AslamCamera();
+
+
+
+};
---
+++ src/kalibr_common/ConfigReader.cpp
@@ -0,0 +1,9 @@
+//
+// Created by radam on 2021-03-23.
+//
+
+#include <kalibr_common/ConfigReader.hpp>
+
+AslamCamera::AslamCamera() {
+
+}
