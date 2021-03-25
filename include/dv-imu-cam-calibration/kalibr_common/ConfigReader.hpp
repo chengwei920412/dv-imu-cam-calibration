dfd344d7fa63891ffa1648018d6582143ee7f2fa
Adam Radomski <adam.radomski@inivation.com>
2021-03-25 15:40:47 +0100

    Config reader kinda finished

--- include/dv-imu-cam-calibration/kalibr_common/ConfigReader.hpp
+++ include/dv-imu-cam-calibration/kalibr_common/ConfigReader.hpp
@@ -8,7 +8,7 @@
 
 #include <vector>
 
-class PinholeCamera {
+class PinholeEquidistantCamera {
 
 public:
 
@@ -17,8 +17,38 @@ protected:
 
 public:
 
-  PinholeCamera(const std::vector<double>& intrinsics,const std::vector<double>& dist_coeff,const cv::Size& resolution);
+  PinholeEquidistantCamera(const std::vector<double>& intrinsics, const std::vector<double>& distCoeff, const cv::Size& resolution);
+
+  static PinholeEquidistantCamera fromParameters() {}; // TODO(radam): implement
+
+
+};
+
+
+struct CameraParameters {
+
+  std::vector<double> intrinsics{604.5911733980397, 604.2336278279186, 282.3605083440955, 250.5144138417647};
+  std::vector<double> distCoeff{-0.05965984963878861, 0.11156790983914057, -0.397476602431665, 0.4856393825761525};
+  cv::Size resolution{640, 480};
 
 
 
 };
+
+struct ImuParameters {
+  double updateRate = 200.0;
+  double accNoiseDensity = 1.4e-3;
+  double accRandomWalk = 8.6e-5;
+  double gyrNoiseDensity = 8.0e-5;
+  double gytRandomWalk = 2.2e-6;
+};
+
+struct AsymmetricCircleGridParameters {
+  std::string targetType{"circlegrid"};
+  size_t targetRows = 4;
+  size_t targetCols = 11;
+  double spacingMeters = 0.05;
+
+};
+
+// TODO(radam): camera chain skipped as we only want one camera
--- src/kalibr_common/ConfigReader.cpp
+++ src/kalibr_common/ConfigReader.cpp
@@ -4,14 +4,26 @@
 
 #include <kalibr_common/ConfigReader.hpp>
 
-//#include <aslam/cameras/EquidistantDistortion.hpp>
+#include <aslam/cameras/EquidistantDistortion.hpp>
 
-PinholeCamera::PinholeCamera(const std::vector<double> &intrinsics,const std::vector<double> &dist_coeff,const cv::Size &resolution) {
+PinholeEquidistantCamera::PinholeEquidistantCamera(const std::vector<double> &intrinsics, const std::vector<double> &distCoeff, const cv::Size &resolution) {
 
   const auto focalLength = std::vector<double>(intrinsics.begin(), intrinsics.begin()+2);
   const auto principalPoint = std::vector<double>(intrinsics.begin()+2, intrinsics.begin()+4);
 
+  assert(distCoeff.size() >= 4);
+  const auto dist = aslam::cameras::EquidistantDistortion(distCoeff.at(0), distCoeff.at(1), distCoeff.at(2), distCoeff.at(3));
+  //const auto proj = aslam::cameras:: // TODO(radam): this is missing
+
+  // geometry = // TODO(radam): missing
+
+  // TODO(radam): everything is missing here...
 
 
+
+}
+
+
+ParametersBase::ParametersBase() {
 
 }
